1be652e6128340dc6893e529054b1a3b7d557403
MuhammadJghz7 <145560831+MuhammadJghz7@users.noreply.github.com>
2025-09-21 08:43:52 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // GSAP Animation for hero text
+    gsap.to('.line', {
+        y: 0,
+        opacity: 1,
+        duration: 1.2,
+        stagger: 0.3, // Adds a delay between each line's animation
+        ease: 'power4.out'
+    });
+});
