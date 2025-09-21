d5a6ef9dfe5afb9e2b41911e4acc6a82cc016c53
MuhammadJghz7 <145560831+MuhammadJghz7@users.noreply.github.com>
2025-09-21 14:02:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
         y: 0,
         opacity: 1,
         duration: 1.2,
-        stagger: 0.3, // Adds a delay between each line's animation
+        stagger: 0.9, // Adds a delay between each line's animation
         ease: 'power4.out'
     });
 });
